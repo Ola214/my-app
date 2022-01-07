f7e9c12846f6f6c2eb4ec15fa3dca524cbdf86ff
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-07 15:40:52 +0100

    css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import './App.css'
 
 class App extends Component {
   render() {
 
     //kod jsx
     return (
-      <h1>Komponent klasowy!</h1>
+      // //inny sposób tworzenia zawartości komponentu
+      // React.createElement('div', null, React.createElement('h1', {className: 'Red-color'}, 'Hello, I am react App')) //typ, obiekt,potomek rodzica
+
+      <div className="App">
+        <h1 className='Red-color'>Hello, I am a react App</h1>
+      </div>
     );
   }
 }

ff24b54c4479a752b21770e76c52429b4b36560a
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-07 20:54:51 +0100

    styling started

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Article.css'
 
 const article = props => {
     return(
-        <div>
+        <div className="Article">
             <h2>{props.title}</h2>
             <p>{props.content}</p>
             <button onClick={props.deleteClick}>Delete article</button>

f4c4f0e3bf644a960db68c48c94335580ccb3298
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-07 16:21:05 +0100

    state creating form

--- src/App.js
+++ src/App.js
@@ -13,8 +13,15 @@ class App extends Component {
       <div className="App">
         <h1 className='Red-color'>Hello, I am a react App</h1>
         <p>Paragraf tekstowy</p>
-        <User username="Szymon1" role="admin">active</User>
-        <User username="FooBar" role="user">inactive</User>
+        <div className="Form-container">
+          <label className="Form-label">Username</label>
+          <input className="Form-element" type="text" id="username" />
+          <label className="Form-label">Role</label>
+          <input className="Form-element" type="text" id="role" />
+          <label className="Form-label">Active</label>
+          <input className="Form-element" type="checkbox" id="active" />
+          <button className="Form-button">Show user</button>
+        </div>
       </div>
     );
   }

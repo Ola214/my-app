2cf267848f99a6301d62b52321f345accc9f0c91
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-08 19:19:36 +0100

    component life cycle props actualization

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, {Component} from "react";
 import Article from './Article/Article';
 
-const articles = props => {
+class Articles extends Component {
+
+  // //do warunkowego zmiany stanu
+  // static getDerivedStateFromProps(props, state){
+  //   console.log("Articles getDerivedStateFromProps");
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("Articles shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log("Articles getSnapshotBeforeUpdate");
+    return {message: "this is test message"}
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("Articles componentDidUpdate");
+    console.log("snapshot message: " + snapshot.message);
+  }
+
+  render() {
+    console.log("Articles render");
     console.log("articles");
 
     const articleStyles = [];
 
-    if(props.articles.length === 1){
+    if(this.props.articles.length === 1){
       articleStyles.push('OneArticle');
     }
 
-    if(props.articles.length >= 4) {
+    if(this.props.articles.length >= 4) {
       articleStyles.push('GreenArticles');
     } else {
       articleStyles.push('OrangeArticles');
     }
 
-    const articles = props.articles.map((article, index) =>{
+    const articles = this.props.articles.map((article, index) =>{
         return(<Article 
           title={article.title} 
           content={article.content} 
-          deleteClick={() => props.deleteArticle(index)}
+          deleteClick={() => this.props.deleteArticle(index)}
           key={article.id}
           />);
       });
@@ -30,6 +54,8 @@ const articles = props => {
             {articles}
         </div>
       );
+
+  }
 }
 
-export default articles;
+export default Articles;

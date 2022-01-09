731b3b12ed23dd5375997f5c30f442853ebb272d
aleksandra-k <aleksandra.krysinska.94@gmail.com>
2022-01-09 11:08:49 +0100

    Auxiliary component best way of using

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Auxiliary from '../Auxiliary/Auxiliary'
 
 const links = () => (
-    [
-        <a href="https://www.google.com">https://www.google.com</a>,
-        <a href="https://www.reactjs.org">https://www.reactjs.org</a>,
+    <React.Fragment>
+        <a href="https://www.google.com">https://www.google.com</a>
+        <a href="https://www.reactjs.org">https://www.reactjs.org</a>
         <a href="https://www.nodejs.org">https://www.nodejs.org</a>
-    ]
+    </React.Fragment>
     
 )
 
